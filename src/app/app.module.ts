b3d61595780b9428eea8fb9a088313724862dcf1
levaered <vova21sergeev@gmail.com>
2023-05-04 23:38:43 +0300

    army

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarriorCardComponent } from './warrior-card/warrior-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarriorCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warrior-card/warrior-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warrior-card',
+  templateUrl: './warrior-card.component.html',
+  styleUrls: ['./warrior-card.component.scss']
+})
+export class WarriorCardComponent {
+
+}
